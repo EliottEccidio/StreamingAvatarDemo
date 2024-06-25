a533aa29bfe9d1c58f851e04ff495418b239b173
Eliott <eliott.eccidio@hotmail.fr>
2024-06-25 15:33:13 +0200

    fixing audio from wav to mp3

--- src/App.tsx
+++ src/App.tsx
@@ -5,50 +5,58 @@ import { useEffect, useRef, useState } from 'react';
 import { Configuration, NewSessionData, StreamingAvatarApi} from '@heygen/streaming-avatar';
 import './App.css';
 import { getToken } from './getToken';
+//import { start } from 'repl';
 
 function App() {
   const [stream, setStream] = useState<MediaStream> ();
   const [debug, setDebug] = useState<string> ();
-  let token;
-  
+  //let avatar = useRef<StreamingAvatarApi | null> (null);
   const fetchToken = async () => {
     const token = await getToken();
     console.log("\n-------_______--------\n Value of the token:"+token+"\n--------------\n")
     return String(token);
   }
-  token = fetchToken();
+  
+  const token = fetchToken();
   console.log("\n--------------\nToken in APP:"+token+"\n\n")
-  
   const avatar = useRef(new StreamingAvatarApi(
-      new Configuration({accessToken: token})
+    new Configuration({accessToken: token})
   ));
-  
-
   const [text, setText] = useState<string>("");
   const [avatarId, setAvatarId] = useState<string>("");
   const [voiceId, setVoiceId] = useState<string>("");
+  const [isRecording, setIsRecording] = useState<boolean>(false); // État pour contrôler la visibilité des boutons
 
   const [data, setData] = useState<NewSessionData>();
   const mediaStream = useRef<HTMLVideoElement>(null);
 
-  async function grab(){
+
+
+  async function useGrab(){
     try{
-      const res = await avatar.current.createStartAvatar(
-      { newSessionRequest: 
-        { quality: "low",
-          avatarName: avatarId, 
-          voice:{voiceId: voiceId}
-        }
-      }, setDebug);
-      setData(res);
-      setStream(avatar.current.mediaStream);
+      
+        if(avatar.current!=null){
+        const res = await avatar.current.createStartAvatar(
+        { newSessionRequest: 
+          { quality: "low",
+            avatarName: avatarId, 
+            voice:{voiceId: voiceId}
+          }
+        }, setDebug);
+        setData(res);
+        setStream(avatar.current.mediaStream);
+      }
     } catch (error) {
 
     }  
   };
 
   async function stop(){
-    await avatar.current.stopAvatar({stopSessionRequest: {sessionId: data?.sessionId}}, setDebug);
+    if (avatar.current && data?.sessionId) {
+      await avatar.current.stopAvatar({stopSessionRequest: {sessionId: data?.sessionId}}).catch((e) => {
+        setDebug(e.message);
+      });
+    }
   }
 
   useEffect(()=>{
@@ -61,11 +69,92 @@ function App() {
     }
   }, [mediaStream, stream])
 
-  async function handleSpeak(){
-    await avatar.current.speak({taskRequest: {text: text, sessionId: data?.sessionId}}).catch((e) => {
-      setDebug(e.message);
-    });
+
+  let mediaRecorder = useRef<MediaRecorder | null>(null); // Utilisation de ref pour mediaRecorder
+  let audioChunks = useRef<Blob[]>([]); // Utilisation de ref pour audioChunks
+
+
+  const startButton = document.getElementById('StartRecording');
+  const stopButton = document.getElementById('StopRecording');
+  
+
+  async function getAudioRecord() {
+    try {
+      setIsRecording(true);
+      if(startButton != null && stopButton!= null){
+        startButton.style.display = 'none';
+        stopButton.style.display = 'inline';
+      }
+
+      // Démarrer l'enregistrement
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      mediaRecorder.current = new MediaRecorder(stream);
+
+      mediaRecorder.current.ondataavailable = (event: BlobEvent) => {
+          audioChunks.current.push(event.data);
+      };
+
+      mediaRecorder.current.start();
+      console.log(" \n\n\n- - -- - - media-Started \n\n\n")
+    } catch(error) {
+
+    }
   }
+
+
+  async function handleAvatar() {
+    if(startButton != null && stopButton!= null){
+      stopButton.style.display = 'none';
+      startButton.style.display = 'inline';
+    }
+
+    // Arrêter l'enregistrement
+    setIsRecording(false);
+    console.log(" \n\n\n- - -- - - media-Strooped \n\n\n")
+    if (mediaRecorder.current!=null){
+      mediaRecorder.current.stop();
+      console.log(" \n\n\n- - -- - - media-Stoped \n\n\n")
+
+
+      mediaRecorder.current.onstop = async () => {
+          const audioBlob = new Blob(audioChunks.current, { type: 'audio/mp3' });
+          
+          // Créez un FormData pour envoyer le fichier audio
+          const formData = new FormData();
+          formData.append('file', audioBlob, 'recording.mp3');
+
+          /* Envoie à L'API */
+          
+          try {
+              const response = await fetch('https://bs-avatar-api-staging-6952e3e70030.herokuapp.com/response', { 
+                  method: 'POST',
+                  body: formData,
+              });
+
+              if (response.ok) {
+                  console.log('Audio successfully sent to the API');
+                  await handleSpeak(response)
+                  console.log("Response of the LLM :", response)
+              } else {
+                  console.error('Error sending audio to the API', response.statusText);
+              }
+          } catch (error) {
+              console.error('Error:', error);
+          }
+          
+          
+      };
+    }
+  }
+
+  async function handleSpeak(response: any){
+    
+    if (avatar.current && data?.sessionId) {
+      await avatar.current.speak({taskRequest: {text: response, sessionId: data?.sessionId}}).catch((e) => {
+        setDebug(e.message);
+      });
+    }
+  }//<input className="InputField" placeholder='Type something for the avatar to say' value={text} onChange={(v)=>setText(v.target.value)}/>
   
   return (
     <div className="HeyGenStreamingAvatar">
@@ -83,9 +172,10 @@ function App() {
           <input className="InputField2" placeholder='Voice ID' value={voiceId} onChange={(v)=>setVoiceId(v.target.value)}/>
         </div>
         <div className="Actions">
-          <input className="InputField" placeholder='Type something for the avatar to say' value={text} onChange={(v)=>setText(v.target.value)}/>
-          <button onClick={handleSpeak}>Speak</button>
-          <button onClick={grab}>Start</button>
+          
+          <button id='startButton' onClick={getAudioRecord} style={{ display: isRecording ? 'none' : 'inline' }}>Start Recording </button>
+          <button id='stopButton' onClick={handleAvatar} style={{ display: isRecording ? 'inline' : 'none' }}>Stop Recording</button>
+          <button onClick={useGrab}>Start</button>
           <button onClick={stop}>Stop</button>
         </div>
         
